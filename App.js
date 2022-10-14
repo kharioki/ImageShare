5979cd3ea06060d25895cc78931ed02e2b062066
kharioki <tony.kharioki@gmail.com>
2022-10-14 16:20:44 +0300

    basic components, image, touchableOpacity and styling

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{ uri: 'https://res.cloudinary.com/khariokitony/image/upload/v1665753309/darth-jarjar.jpg' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo press the button below!
+      </Text>
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
